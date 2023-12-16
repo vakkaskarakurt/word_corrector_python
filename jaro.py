325ee5e39629cafdb0c3c00f06e34b7f37cdb105
vakkaskarakurt <karakurtvakkas@gmail.com>
2023-12-16 20:43:22 +0300

    .

--- jaro.py
+++ jaro.py
@@ -2,8 +2,8 @@ from textdistance import jaro
 from textdistance import jaro_winkler
 
 
-def jaro_similarity(kelime1, kelime2):
-    jaro_sim = jaro_winkler(kelime1, kelime2)
+def jaro_similarity(word1, word2):
+    jaro_sim = jaro_winkler(word1, word2)
     if jaro_sim == 0:
         jaro_sim = 0.0000001
     return jaro_sim
